7473437712d3263d162639de389782fc484e9c41
Ge6ben <dl22gesh40@gmail.com>
2021-08-30 17:40:51 +0300

    Redux Lab

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+import { createStore } from "redux";
 import usersReducer from "./features/users/usersSlice";
-
-// add imports and code
+import { Provider } from "react-redux";
+const store =createStore(usersReducer);
 
 ReactDOM.render(
+<Provider store={store}>
+<App />   
+</Provider>
   // add imports and code
-  <App />,
+ ,
   // add imports and code
   document.getElementById("root")
 );
